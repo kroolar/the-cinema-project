3ecda6264723c6d97130e018544b4f07b010e572
Artur Krol <kroolar@gmail.com>
2024-11-04 17:00:05 +0100

    Add button to movies page

--- app/components/controller/controller_component.rb
+++ app/components/controller/controller_component.rb
@@ -1,7 +1,7 @@
 class Controller::ControllerComponent < ViewComponent::Base
   attr_reader :class_name, :name
 
-  def initialize(name)
-    @name = name
+  def initialize(name:)
+    @name = name.to_s
   end
 end
